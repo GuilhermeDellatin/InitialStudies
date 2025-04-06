011862105eae305ef6a2fcf2328d6aeab6118e3a
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-06 19:38:38 -0300

    First Commit

---
+++ basics/introducao_a_programacao_com_python/cap_2.py
@@ -0,0 +1,21 @@
+# Exercício 2.1
+# Converta as expressões matemáticas para que possam ser calculadas
+# usando o interpretador Python.
+#
+# 10 + 20 x 30
+# 4² + 30
+#(9^4 + 2) x 6 - 1
+
+#1 - Resposta: Para a expressão 10 + 20 * 30 = 610
+#2 - Resposta: Para a expressão 4 ** 2 + 30 = 46
+#3 - Resposta: Para a expressão (9 ** 4 + 2) * 6 - 1 = 39377
+
+# Exercício 2.2
+# Digite a seguinte expressão no interpretador:
+#
+# 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+#
+# Tente resolver o mesmo cálculo, usando lápis e papel.
+# Observe como a prioridade das operações é importante.
+
+# Resposta: Para a expresão 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2 = 81.0
