f0476e912f819e1979bdfed5bac2a4e9868cb7cf
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-06 20:04:38 -0300

    Solve some exercises

--- basics/introducao_a_programacao_com_python/cap_2.py
+++ basics/introducao_a_programacao_com_python/cap_2.py
@@ -19,3 +19,41 @@
 # Observe como a prioridade das operações é importante.
 
 # Resposta: Para a expresão 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2 = 81.0
+
+# Exercício 2.3
+# Faça um programa que exiba seu nome na tela.
+
+print("Guilherme Fernandes Dellatin")
+
+# Exercício 2.4
+# Escreva um programa que exiba o resultado de 2a x 3b
+# onde a vale 3 e b vale 5.
+a = 3
+b = 5
+result = (2 * a) * (3 * b)
+print(result)
+
+# Exercício 2.5
+# Modifique o primeiro programa, listagem 2.7, de forma a calcular
+# a soma de três variáveis.
+#
+# a = 2
+# b = 3
+# print(a + b)
+number1 = 2
+number2 = 3
+number3 = 4
+result_sum_numbers = number1 + number2 + number3
+print(result_sum_numbers)
+
+# Exercício 2.6
+# Modifique o programa da listagem 2.11, de forma que ele calcule um aumento de 15%
+# para um salário de R$ 750.
+#
+# 2.11
+# salary = 1500
+# increase = 5
+# print(salary + (salary * increase / 100))
+salary = 750
+increase = 15
+print(salary + (salary * increase / 100))
