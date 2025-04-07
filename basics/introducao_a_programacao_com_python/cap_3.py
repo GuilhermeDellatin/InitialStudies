c08e693fa0bfd9cfb0d1ec53cbe8452754955e55
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-07 08:41:23 -0300

    Solve some exercises

---
+++ basics/introducao_a_programacao_com_python/cap_3.py
@@ -0,0 +1,53 @@
+# Exercício 3.1
+# Complete a tabela a seguir, marcando inteiro ou ponto flutuante
+# dependendo do número  apresentado.
+#
+# Número    Tipo numérico
+#   5         () int () float
+#   5.0       () int () float
+#   4.3       () int () float
+#   -2        () int () float
+#   100       () int () float
+#   1.333     () int () float
+
+# Resposta:
+#
+# Número    Tipo numérico
+#   5         (x) int () float
+#   5.0       () int (x) float
+#   4.3       () int (x) float
+#   -2        (x) int () float
+#   100       (x) int () float
+#   1.333     () int (x) float
+
+# Exercício 3.2
+# Complete a tabela abaixo, respondendo True ou False.
+# Considere a = 4, b = 10, c = 5.0, d = 1 e f = 5.
+#
+#   Expressão        Resultado
+#     a == c     () True () False
+#     a < b      () True () False
+#     d > b      () True () False
+#     c! = f     () True () False
+#     a == b     () True () False
+#     c < d      () True () False
+#     b > a      () True () False
+#     c >= f     () True () False
+#     f >= c     () True () False
+#     c <= c     () True () False
+#     c <= f     () True () False
+
+# Resposta:
+#
+#   Expressão        Resultado
+#     a == c     () True (x) False
+#     a < b      (x) True () False
+#     d > b      () True (x) False
+#     c != f     () True (x) False
+#     a == b     () True (x) False
+#     c < d      () True (x) False
+#     b > a      (x) True () False
+#     c >= f     (x) True () False
+#     f >= c     (x) True () False
+#     c <= c     (x) True () False
+#     c <= f     (x) True () False
