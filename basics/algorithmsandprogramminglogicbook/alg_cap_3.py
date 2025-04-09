efa914958063bcc9ea7ba1447bbfeb2c13d52d52
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-09 08:43:58 -0300

    Create others exercíses descriptions

---
+++ basics/algorithmsandprogramminglogicbook/alg_cap_3.py
@@ -0,0 +1,16 @@
+# Exercício 3.1
+# Elabore um algoritmo que calculem quantas notas de 50, 10 e 1 são necessárias para pagar uma conta
+# cujo valor é fornecido.
+
+# Exercício  3.5
+# Elabore um algoritmo que calcule o alcance de um projetil, dada a velocidade inicial v0 e  angulo Theta entre o
+# cano do canhão e o solo. A fórmula a ser utilizada é:
+# S = (v0²/g) * sen(2teta)
+
+# Exercício 3.6
+# Elabore um algoritmo que calcula a área de um triângulo pela fórmula de Hierão:
+# K = SQRT(s(s - a) * (s - b) * (s - c))
+# Em que K é a área do triângulo, s o semiperímetro e a, b e c os lados do triângulo.
+
+# Exercício 3.7
+# Elabore um algoritmo que permitam a entrada de um número inteiro e diga se ele é par ou ímpar.
