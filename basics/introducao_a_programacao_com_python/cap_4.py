ba3906b9aed32c2fb94d98bc55dbbd2443ef285c
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-10 07:44:16 -0300

    Solve some cap 4 exercises

---
+++ basics/introducao_a_programacao_com_python/cap_4.py
@@ -0,0 +1,22 @@
+# Listagem 4.2 - Condições
+#
+# a = int(input("Primeiro Valor: "))
+# b = int(input("Segundo Valor: "))
+# if a > b:
+#   print("O primeiro número é o maior!")
+# if b > a:
+#   print("O segundo número é o maior!")
+
+# Exercício 4.1
+# Analise o programa da listagem 4.2. Responda o que acontece
+# se o primeiro e o segundo valores forem iguais? Explique.
+
+a = int(input("Primeiro Valor: "))
+b = int(input("Segundo Valor: "))
+if a > b:
+    print("O primeiro número é o maior!")
+if b > a:
+    print("O segundo número é o maior!")
+
+# Resposta:
+# Quando os dois valores são iguais nenhuma instrução print é executada pois não satisfazem as condições if
