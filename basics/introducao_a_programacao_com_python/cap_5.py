37ab4e6a17e1766385ed68956d0132ee93cd14b0
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-13 09:46:24 -0300

    Solve 5.4 cap 5 exercises using while

--- basics/introducao_a_programacao_com_python/cap_5.py
+++ basics/introducao_a_programacao_com_python/cap_5.py
@@ -33,3 +33,38 @@ while countdown >= 0:
     countdown = countdown - 1
 
 print("Fire!")
+
+# Exemplos
+#
+# fim = int(input("Digite o último número a imprimir:"))
+# x = 0
+# while x <= fim:
+#   if x % 2 == 0:
+#       print(x)
+#   x = x + 1
+#
+# fim = int(input("Digite o último número a imprimir:"))
+# x = 0
+# while x <= fim:
+#   print(x)
+#   x = x + 2
+
+# Exercício 5.4
+# Modifique o programa anterior para imprimir de 1 até o número digitado pelo usuário, mas,
+# dessa vez, apenas os números ímpares.
+
+end = int(input("Enter the last number to print: "))
+number = 1
+while number <= end:
+    if number % 2 != 0:
+        print(number)
+    number = number + 1
+
+end = int(input("Enter the last number to print: "))
+number = 1
+while number <= end:
+    print(number)
+    number = number + 2
+
+# Exercício 5.5
+# Reescreva o programa anterior para escrever os 10 primeiros múltiplos de 3.
