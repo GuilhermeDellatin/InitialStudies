269b5e3eaed03632b6d220ed19c8eefe8e5b1bfc
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-14 21:32:59 -0300

    Solve exercises 5.20 cap 5 exercises using while

--- basics/introducao_a_programacao_com_python/cap_5.py
+++ basics/introducao_a_programacao_com_python/cap_5.py
@@ -416,3 +416,40 @@ while True:
 # Exercício 05.20
 # O que acontece se digitarmos 0,001 no programa anterior? Caso ele não funcione, altere-o de forma a
 # corrigir o problema.
+
+value = float(input("Enter the value to pay: "))
+cedulas = 0
+actual = 100
+to_pay = value
+
+while True:
+    if actual <= to_pay:
+        to_pay -= actual
+        cedulas += 1
+    else:
+        print(f"{cedulas} cedula(s) de R${actual}")
+        if to_pay < 0.001:
+            break
+        if actual == 100:
+            actual = 50
+        elif actual == 50:
+            actual = 20
+        elif actual == 20:
+            actual = 10
+        elif actual == 10:
+            actual = 5
+        elif actual == 5:
+            actual = 1
+        elif actual == 1:
+            actual = 0.50
+        elif actual == 0.50:
+            actual = 0.10
+        elif actual == 0.10:
+            actual = 0.05
+        elif actual == 0.05:
+            actual = 0.02
+        elif actual == 0.02:
+            actual = 0.01
+        elif actual == 0.01:
+            actual = 0.001
+        cedulas = 0
