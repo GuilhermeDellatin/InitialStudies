a8dd66b80c6f7786da524bd8f85875e8f3d88367
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-20 10:36:02 -0300

    Solve exercise 6.1 cap 6 exercises using list

--- basics/introducao_a_programacao_com_python/cap_6.py
+++ basics/introducao_a_programacao_com_python/cap_6.py
@@ -22,3 +22,23 @@
 #    print(f"Nota {x}: {notas[x]:6.2f}")
 #    x += 1
 #print(f"Media: {soma / x:5.2f}")
+
+# Exercício 6.1
+# Modifique o Programa 6.2 para ler 7 notas em vez de 5.
+
+grades = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+sum_grades = 0
+grade = 0
+
+while grade < 7:
+    grades[grade] = float(input(f"Grade {grade}: "))
+    sum_grades += grades[grade]
+    grade += 1
+
+grade = 0
+
+while grade < 7:
+    print(f"Grade {grade}: {grades[grade]:6.2f}")
+    grade += 1
+
+print(f"Average: {sum_grades / grade:5.2f}")
