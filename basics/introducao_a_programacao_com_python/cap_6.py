404f05d415b343cd9e23f81966980b80eb5065db
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-21 20:22:08 -0300

    Example 6.8

--- basics/introducao_a_programacao_com_python/cap_6.py
+++ basics/introducao_a_programacao_com_python/cap_6.py
@@ -284,3 +284,28 @@ while True:
         break
     else:
         print("Operação inválida! Digite apenas F, a ou S!")
+
+# Programa 6.8 Pilha de pratos
+
+prato = 5
+pilha = list(range(1, prato + 1))
+
+while True:
+    print(f"\nExistem {len(pilha)} pratos na pilha")
+    print(f"Pilha atual: {pilha}")
+    print("Digite E para empilhar um novo prato,")
+    print("ou D para desempilhar. S para sair.")
+    operacao = input("Operação (E, D ou S):")
+    if operacao == "D":
+        if len(pilha) > 0:
+            lavado = pilha.pop(-1)
+            print(f"Prato {lavado} lavado")
+        else:
+            print("Pilha vazia! Nada para lavar.")
+    elif operacao == "E":
+        prato += 1
+        pilha.append(prato)
+    elif operation == "S":
+        break
+    else:
+        print(f"Operação inválida! Digite apenas E, D ou S!")
