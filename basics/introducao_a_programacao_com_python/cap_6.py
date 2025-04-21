383c8875e8b8834a2c029e11978ca766bf9be1ce
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-21 20:25:56 -0300

    Exercise 6.7 description

--- basics/introducao_a_programacao_com_python/cap_6.py
+++ basics/introducao_a_programacao_com_python/cap_6.py
@@ -309,3 +309,16 @@ while True:
         break
     else:
         print(f"Operação inválida! Digite apenas E, D ou S!")
+
+# Exercício 6.7
+# Faça um programa que leia uma expressão com parênteses. Usando pilhas,
+# verifique se os parênteses foram abertos e fechados na ordem correta.
+#
+# Exemplo:
+# (()) OK
+# ()()(()()) OK
+# ()) Erro
+#
+# Você pode adicionar elementos à pilha sempre que encontrar abre parênteses e desempilhá-la
+# a cada fecha parênteses. Ao desempilhar, verifique se o topo da pilha é um abre parênteses.
+# Se a expressão estiver correta, sua pilha estará vazia no final.
