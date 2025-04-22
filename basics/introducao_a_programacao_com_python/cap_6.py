1a4ba895fd155be4ed5f1003c17af9dd195a4140
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-22 08:11:43 -0300

    Sample 6.9

--- basics/introducao_a_programacao_com_python/cap_6.py
+++ basics/introducao_a_programacao_com_python/cap_6.py
@@ -344,3 +344,27 @@ if len(stack) == 0:
     print("The expression is correct!")
 else:
     print("The expression is wrong!")
+
+# Programa 6.9 Pesquisa sequencial
+"""
+L = [15, 7, 27, 39]
+p = int(input("Digite o valor a procurar:"))
+achou = False
+x = 0
+
+while x < len(L):
+    if L[x] == p:
+        achou = True
+        break
+    x += 1
+
+if achou:
+    print(f"{p} achado na posição {x}")
+else:
+    print(f"{p} nao encontrado")
+"""
+
+# Exercício 6.8
+# Modifique o primeiro exemplo (Programa 6.9) de forma a realizar a mesma tarefa,
+# mas sem utilizar a variável achou. Dica: observe a condição de saída do while.
+
