d2019a9dadcb467467330b09dd0936961e47d24c
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-23 07:55:39 -0300

    Solve exercise 6.11 cap 6 exercises using list with for loop

--- basics/introducao_a_programacao_com_python/cap_6.py
+++ basics/introducao_a_programacao_com_python/cap_6.py
@@ -431,6 +431,7 @@ else:
 
 # Exemplo pesquisa sequencial usando for
 
+"""
 L = [7,  9, 10, 12]
 p = int(input("Digite um número a pesquisar:"))
 
@@ -440,3 +441,38 @@ for e in L:
         break
 else:
     print("Elemento nao encontrado")
+"""
+
+# Programa 6.6 Adição de elementos a lista
+
+"""
+L = []
+while True:
+    n = int(input("Digite um número (0 sai):"))
+    if n == 0:
+        break
+    L.append(n)
+x = 0
+while x < len(L):
+    print(L[x])
+    x += 1
+"""
+
+# Exercício 6.11
+# Modifique o programa 6.6 usando for. Explique por que nem todos os while
+# podem ser transformados em for.
+
+number_list = []
+
+while True:
+    n = int(input("Digite um número (0 sai):"))
+    if n == 0:
+        break
+    number_list.append(n)
+x = 0
+
+for e in number_list:
+    print(e)
+
+# Resposta: O primeiro while não pode ser substituído, porque temos uma condição especifíca para sair do laço de
+# repetição, nesse caso precisamos popular a lista de forma dinâmica, sem ter um tamanho pré-definido.
