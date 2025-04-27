af3d4756027e84d74c4e43532ec572cfe4ca918d
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-27 17:17:02 -0300

    Example 6.16

--- basics/introducao_a_programacao_com_python/cap_6.py
+++ basics/introducao_a_programacao_com_python/cap_6.py
@@ -775,3 +775,11 @@ for s in L:
     for letra in s:
         print(letra)
 """
+
+# Programa 6.16 Listas com elementos de tipos diferentes
+
+"""
+produto1 = ["maça", 10, 0.30]
+produto2 = ["pera", 5, 0.75]
+produto3 = ["kiwi", 4, 0.98]
+"""
