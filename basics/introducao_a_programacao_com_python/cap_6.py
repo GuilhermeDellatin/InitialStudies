423eb4861436bd9a003d7e48c58f515e0d4cf4c1
GuilhermeDellatin <guidellatin@gmail.com>
2025-04-27 17:18:56 -0300

    Example 6.17

--- basics/introducao_a_programacao_com_python/cap_6.py
+++ basics/introducao_a_programacao_com_python/cap_6.py
@@ -783,3 +783,13 @@ produto1 = ["maça", 10, 0.30]
 produto2 = ["pera", 5, 0.75]
 produto3 = ["kiwi", 4, 0.98]
 """
+
+# Programa 6.17 Listas de listas
+
+"""
+produto1 = ["maça", 10, 0.30]
+produto2 = ["pera", 5, 0.75]
+produto3 = ["kiwi", 4, 0.98]
+compras = [produto1, produto2, produto3]
+print(compras)
+"""
