ab1d6800ab7d23b542523e8834bca96952f9d823
GuilhermeDellatin <guidellatin@gmail.com>
2025-05-02 18:58:00 -0300

    Solve cap1 other book exercises

--- basics/fundamentos_de_python_primeiros_programas/cap_1.py
+++ basics/fundamentos_de_python_primeiros_programas/cap_1.py
@@ -45,7 +45,15 @@ print("Your name is", name)
 # Carregue o programa no shell pressionando a tecla F5 e corrija os erros que ocorrerem.
 # Teste o programa com diferentes entradas executando-o pelo menos três vezes.
 
+width = int(input("Enter with width: "))
+height = int(input("Enter with height: "))
+area = width * height
+print(f"The area is {area} square units")
 
+# Resposta:
+# Teste 1 width 100 height 80 => 8000 square units
+# Teste 2 width 10 height 5 => 50 square units
+# Teste 3 width 32 height 25 => 800 square units
 
 # Exercício 5
 # Modifique o programa do Projeto 4 para calcular a área de um triângulo. Emita os prompts
@@ -53,25 +61,56 @@ print("Your name is", name)
 # Em seguida, utilize a fórmula .5 * base * height para calcular a área. Teste o programa a
 # partir de uma janela IDLE.
 
+triangle_width = float(input("Enter with triangle width: "))
+triangle_height = float(input("Enter with triangle height: "))
+triangle_area = triangle_width * triangle_height * .5
+print(f"The triangle area is {triangle_area} square units")
+
+# Também podemos usar
+# triangle_area = (triangle_width * triangle_height) / 2
+
 # Exercício 6
 # Escreve e teste um programa que calcula a área de um círculo. Esse programa deve solicitar
 # um número representando um raio como entrada do usuário. Ele deve utilizar a fórmula
 # 3.14 * radius ** 2 para calcular a área e, em seguida, produzir o resultado devidamente
 # rotulado.
+radius = float(input("Enter the radius to calcule circle area: "))
+circle_area = 3.14 * (radius ** 2)
+print(f"The circle area is {circle_area}")
 
 # Exercício 7
 # Escreva e teste um programa que aceite o nome do usuário (como texto) e a idade (como um número)
 # como entrada. O programa deve produzir uma sentença contendo o nome e a idade do usuário.
+
+user_name = input("Enter the user name: ")
+user_age = int(input("Enter the user age: "))
+print(f"The user {user_name} has {user_age} age")
 
 # Exercício 8
 # Insira uma instrução de entrada utilizando a função input no prompt de shell. Quando o prompt
 # solicitar a entrada, digite um número. Em seguida, tente adicionar 1 a esse número, observe
 # os resultados e explique o que aconteceu.
 
+#number = input("Enter a number")
+#number + 1
+# Ocorre erro de tipagem
+# Traceback (most recent call last):
+#   File "<input>", line 1, in <module>
+# TypeError: can only concatenate str (not "int") to str
+
+# caso number seja number = int(input("Enter a number")) podemos adicionar tranquilamente um numero.
+
 # Exercício 9
 # Insira uma instrução de entrada utilizando a função input no prompt do shell. Quando o prompt
 # solicitar a entrada, digite seu primeiro nome, observe os resultados e explique o que aconteceu.
 
+name = input("Enter a name:")
+# Quando digito um nome, por exemplo, "Python" ele é atribuido a variavel name, se eu digitar name é mostrado no shell
+# o conteúdo do nome
+
 # Exercício 10
 # Digite a expressão help() no prompt de shell. Siga as instruções para navegar pelos tópicos
 # e módulos.
+
+# Resposta: Exibe uma central de ajuda na linha de comando, contendo links de documentação
+# alguns comandos e mais...
