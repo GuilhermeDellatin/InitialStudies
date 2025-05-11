f6afed0bf927123060e60122202a2a97f8fbd7c6
GuilhermeDellatin <guidellatin@gmail.com>
2025-05-11 10:00:47 -0300

    Adding description of exercises cap 7

---
+++ basics/introducao_a_programacao_com_python/cap_7.py
@@ -0,0 +1,45 @@
+# Exercício 7.1
+# Escreva um programa que leia duas strings. Verifique se a segunda ocorre dentro da primeira e imprima
+# a posição de início.
+# 1.ª string: AABBEFAATT
+# 2.ª string: BE
+# Resultado: BE encontrado na posição 3 de AABBEFAATT
+
+# Exercício 7.2
+# Escreva um programa que leia duas strings e gere uma terceira com os caracteres comuns às duas strings lidas.
+# 1.ª string: AAACTBF
+# 2.ª string: CBT
+# Resultado: CBT A ordem dos caracteres da string gerada não é importante,
+# mas deve conter todas as letras comuns a ambas.
+
+# Exercício 7.3
+# Escreva um programa que leia duas strings e gere uma terceira apenas com os caracteres que aparecem
+# em uma delas.
+# 1.ª string: CTA
+# 2.ª string: ABC
+# 3.ª string: BT
+# A ordem dos caracteres da terceira string não é importante.
+
+# Exercício 7.4
+# Escreva um programa que leia uma string e imprima quantas vezes cada caractere aparece nessa string.
+# String: TTAAC Resultado: T: 2x A: 2x C: 1x
+
+# Exercício 7.5
+# Escreva um programa que leia duas strings e gere uma terceira, na qual os caracteres da segunda foram
+# retirados da primeira.
+# 1.ª string: AATTGGAA
+# 2.ª string: TG
+# 3.ª string: AAAA
+
+# Exercício 7.6
+# Escreva um programa que leia três strings. Imprima o resultado da substituição na primeira, dos
+# caracteres da segunda pelos da terceira.
+# 1.ª string: AATTCGAA
+# 2.ª string: TG
+# 3.ª string: AC Resultado:
+# AAAACCAA
+
+# Exercício 7.7
+# Escreva um programa que peça ao usuário que digite uma frase e imprima quantas vogais ela contém.
+# Não considere maiúsculas e minúsculas como diferentes. Exemplo: uma frase como “A casa” deve
+# imprimir três “as”.
